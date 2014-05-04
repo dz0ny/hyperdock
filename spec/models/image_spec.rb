76801000afea78896bcee4d6a3c4b6f800772e0e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-03 21:24:33 -0700

    add comments in image spec

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Image do
   let(:image) { build(:image) }
+
+  #it "#docker_index must exist on the official docker index" do
+  #  image.docker_index = "invalid"
+  #  image.should_not be_valid
+
+  #  image.docker_index = "valid"
+  #  image.should be_valid
+  #end
+
 end
