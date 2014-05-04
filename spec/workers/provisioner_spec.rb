20f586bba94a1e94058611e00ec758a6b2d7eed7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-03 20:23:00 -0700

    reach good point of failure in provisioner spec

--- spec/workers/provisioner_spec.rb
+++ spec/workers/provisioner_spec.rb
@@ -1,5 +1,19 @@
+require 'spec_helper'
+
 describe Provisioner do
-  it "spec_name" do
-    subject.should be_a Provisioner
+  let(:worker) { Provisioner.new }
+
+  before(:each) do
+    host = build :host
+    @get_info_stub = stub_get_info host
+    @container = create(:container, host: host)
+  end
+
+  after(:each) do
+    remove_request_stub(@get_info_stub)
+  end
+
+  it "provisions a container" do
+    worker.perform(@container.id)
   end
 end
