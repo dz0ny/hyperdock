f0838a42c2c9498df3dd2b990cf11dc6d776393f
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-03 21:41:27 -0700

    cleanup

--- app/models/host.rb
+++ app/models/host.rb
@@ -5,28 +5,23 @@ class Host < ActiveRecord::Base
   validates_with DockerHostValidator
   has_many :containers
 
-  def docker_url
-    "http://#{self.ip_address}:#{self.port.to_s}"
-  end
-
   validates :name, presence: true
   validates :ip_address, :format => { :with => Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex) }, uniqueness: { scope: :port, message: "and port belong to another host" }
   validates :port, numericality: { only_integer: true, less_than_or_equal_to: 65535, greater_than: 0  }
 
-  def get_info
-    uri = URI.join(self.docker_url, "/info")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.open_timeout = 2
-    http.read_timeout = 2
-    res = http.get(uri.request_uri)
-    JSON.parse(res.body)
+  def info
+    JSON.pretty_generate(docker.info) rescue "None"
   end
 
-  def info
-    JSON.pretty_generate(get_info) rescue "None"
+  def docker_url
+    "http://#{self.ip_address}:#{self.port.to_s}"
+  end
+
+  def get_info
+    docker.info
   end
 
   def docker
-    @docker ||= Docker.new(self)
+    @docker ||= Docker.new(docker_url)
   end
 end
--- lib/docker.rb
+++ lib/docker.rb
@@ -3,12 +3,23 @@
 # Communicates over the network
 # Add security patterns, etc here
 class Docker
-  def initialize host
-    @host = host  
+  attr_reader :base_uri
+
+  def initialize base_uri
+    @base_uri = base_uri
+  end
+
+  def info
+    uri = URI.join(base_uri, "/info")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.open_timeout = 2
+    http.read_timeout = 2
+    res = http.get(uri.request_uri)
+    JSON.parse(res.body)
   end
 
   def pull image
-    uri = URI.join(@host.docker_url, "/images/create")
+    uri = URI.join(base_uri, "/images/create")
     http = Net::HTTP.new(uri.host, uri.port)
     http.request_post(uri.request_uri, "fromImage=#{image.docker_index}") do |response|
       response.read_body do |chunk|
@@ -18,7 +29,7 @@ class Docker
   end
 
   def run image
-    uri = URI.join(@host.docker_url, "/containers/create")
+    uri = URI.join(base_uri, "/containers/create")
     req = Net::HTTP::Post.new(uri)
     req["Content-Type"] = "application/json"
     http = Net::HTTP.new(uri.host, uri.port)
