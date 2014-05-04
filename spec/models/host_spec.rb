fe7441f2f5137bf93877751c73b166dceb5c0ace
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-03 18:09:20 -0700

    improve api validation tests

--- spec/models/host_spec.rb
+++ spec/models/host_spec.rb
@@ -7,13 +7,15 @@ describe Host do
     subject.docker.should be_a Docker
   end
 
-  it "is invalid if #get_info is wrong" do
+  it "is invalid if #get_info does not have key containers" do
     stub_get_info host, false
+    host.get_info.should_not have_key "Containers"
     host.should_not be_valid
   end
 
-  it "is valid if #get_info is right" do
+  it "is valid if #get_info has key containers" do
     stub_get_info host
+    host.get_info.should have_key "Containers"
     host.should be_valid
   end
 end
