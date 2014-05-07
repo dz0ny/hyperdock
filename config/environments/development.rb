f311a7a192f1910c4e8039528026fc8521f17ccb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-07 05:40:51 -0700

    set default url opts for dev env properly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   config.action_mailer.smtp_settings = {}
-  config.action_mailer.default_url_options = { host: 'hyperdock.io', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
