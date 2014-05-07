eb814bfbcd110115bb0d189ff3a4df07698e6235
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-07 02:41:17 -0700

    update hosts factory

--- spec/factories/hosts.rb
+++ spec/factories/hosts.rb
@@ -6,5 +6,12 @@ FactoryGirl.define do
     ip_address "10.0.0.12"
     port 5544
     association :region
+    after(:build) do |host|
+      host.region = create(:region)
+    end
+    after(:create) do |host|
+      host.region.hosts << host
+      host.region.save
+    end
   end
 end
