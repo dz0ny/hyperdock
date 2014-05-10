8d4834db47427dc1832e910ffa2905cd509756fd
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-09 21:35:53 -0700

    rework seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,77 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: "admin@hyperdock.io", password: "12345678", role: "admin", invitation_limit: 100, container_limit: 100)
 
-ny = Region.create(name: "New York")
-ny.hosts.create({
-  name: "ny-01.hyperdock.io",
-  ip_address: "162.243.161.151",
-  port: "5422"
-})
-
-sf = Region.create(name: "San Francisco")
-sf.hosts.create({
-  name: "sf-01.hyperdock.io",
-  ip_address: "107.170.249.184",
-  port: "5422"
-})
-
 am = Region.create(name: "Amsterdam")
-am.hosts.create({
-  name: "am-01.hyperdock.io",
-  ip_address: "188.226.227.62",
-  port: "5422"
-})
+ny = Region.create(name: "New York")
+sf = Region.create(name: "San Francisco")
 
-Image.create({
-  name: "Sinopia",
-  description: "NPM Caching Proxy",
-  docker_index: "keyvanfatehi/docker-sinopia",
-  port_bindings: "4873"
-})
+if Rails.env.production?
+  ny.hosts.create({
+    name: "ny-01.hyperdock.io",
+    ip_address: "162.243.161.151",
+    port: "5422"
+  })
+  sf.hosts.create({
+    name: "sf-01.hyperdock.io",
+    ip_address: "107.170.249.184",
+    port: "5422"
+  })
+  am.hosts.create({
+    name: "am-01.hyperdock.io",
+    ip_address: "188.226.227.62",
+    port: "5422"
+  })
+else
+  sf.hosts.create({
+    name: "test.hyperdock.io",
+    ip_address: "198.199.112.194",
+    port: "5422"
+  })
+end
 
-Image.create({
-  name: "MongoDB",
-  description: "NoSQL Database",
-  docker_index: "dockerfile/mongodb",
-  port_bindings: "27017, 28017"
-})
-
-Image.create({
-  name: "Redis",
-  description: "In-Memory Key-Value Store",
-  docker_index: "dockerfile/redis",
-  port_bindings: "6379"
-})
+##
+# This block of images is commented out because they need
+# configuration options to be broken out to the hyperdock level
+# Image.create({
+#   name: "Sinopia",
+#   description: "NPM Caching Proxy",
+#   docker_index: "keyvanfatehi/docker-sinopia",
+#   port_bindings: "4873"
+# })
+# 
+# Image.create({
+#   name: "MongoDB",
+#   description: "NoSQL Database",
+#   docker_index: "dockerfile/mongodb",
+#   port_bindings: "27017, 28017"
+# })
+# 
+# Image.create({
+#   name: "Redis",
+#   description: "Key-value Store",
+#   docker_index: "dockerfile/redis",
+#   port_bindings: "6379"
+# })
 
 Image.create({
   name: "PostgreSQL",
-  description: "In-Memory Key-Value Store",
-  docker_index: "dockerfile/redis",
-  port_bindings: "6379"
+  description: "SQL Database",
+  docker_index: "orchardup/docker-postgresql",
+  port_bindings: "5432",
+  env_defaults: [
+    POSTGRESQL_USER: "my_user",
+    POSTGRESQL_PASS: "my_password",
+    POSTGRESQL_DB: "my_database"
+  ]
 })
 
 Image.create({
   name: "ZNC",
   description: "IRC Bouncer",
   docker_index: "hyperdock/znc",
-  port_bindings: "36660"
+  port_bindings: "36660",
+  env_defaults: [
+    ZNC_USER: "my_admin",
+    ZNC_PASS: "my_password"
+  ]
 })
 
