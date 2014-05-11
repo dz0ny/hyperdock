bbedb27fccbcb2ef58e2671cff8a5ef50cad7df9
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-11 02:58:58 -0700

    implement extract_host_port helper

--- app/helpers/containers_helper.rb
+++ app/helpers/containers_helper.rb
@@ -2,4 +2,8 @@ module ContainersHelper
   def simplify_container_port_bindings json_string
     JSON.parse(json_string).map {|port, pair| pair[0]["HostPort"]+"/"+port.split('/')[1] }.join(', ') rescue "N/A"
   end
+
+  def extract_host_port port_pair_value
+    port_pair_value[0]['HostPort'] rescue false
+  end
 end
