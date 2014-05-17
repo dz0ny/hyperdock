ec0029279d0e7d85269bbf88c7985f9c1770ed43
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-17 16:59:44 -0700

    validate ip address and name

--- lib/ssh_wrapper.rb
+++ lib/ssh_wrapper.rb
@@ -1,13 +1,24 @@
 require 'net/ssh'
 require 'net/scp'
+require "resolv"
 
 class SshWrapper
   attr_accessor :ssh
+  NAME_PATTERN = /^[a-zA-Z0-9][a-zA-Z\-\_0-9]*[a-zA-Z0-9]$/
 
-  def initialize host, user="root", password
-    @host = host
+  def initialize ip, user="root", password, name
+    if ip =~ Resolv::IPv4::Regex
+      @host = ip
+    else
+      raise "#{ip} is not a valid IPv4 address"
+    end
     @user = user
     @password = password
+    if name.match(NAME_PATTERN)
+      @name = name
+    else
+      raise "name was not valid. pattern: #{NAME_PATTERN}"
+    end
   end
 
   protected
@@ -23,7 +34,7 @@ class SshWrapper
   def connect
     begin
       Net::SSH.start(@host, 'root', password: ENV['password']) do |ssh|
-        log "Connected!"
+        log "Connected to #{@name}"
         @ssh = ssh
         yield
       end
