273a1dd97c5413fcdc9aefc98b8519c1678135ef
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-18 16:07:29 -0700

    add postinstall and refactor

--- lib/hyperdock/sensu_monitor_setup.rb
+++ lib/hyperdock/sensu_monitor_setup.rb
@@ -3,8 +3,27 @@ require 'hyperdock/sensu_setup_common'
 module Hyperdock
   module SensuMonitorSetup
     include SensuSetupCommon
-    DIR = SENSU_CONFIG_DIR.join('monitor')
-    CERT_TAR = DIR.join('ssl_certs.tar')
+    MONITOR_DIR = SENSU_CONFIG_DIR.join('monitor')
+    CERT_TAR = MONITOR_DIR.join('ssl_certs.tar')
+    RABBITMQ_INSTALL_SCRIPT = <<-EOF
+      apt-get -y install erlang-nox
+      wget -q http://www.rabbitmq.com/rabbitmq-signing-key-public.asc -O- | apt-key add -
+      echo "deb     http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
+      apt-get update
+      export DEBIAN_FRONTEND=noninteractive
+      apt-get install rabbitmq-server
+    EOF
+    RABBITMQ_POSTINSTALL_SCRIPT = {
+      "Creating SSL directory on RabbitMQ server" => "mkdir -p /etc/rabbitmq/ssl",
+      "Copying generated SSL files for RabbitMQ server" => %{
+        cp /tmp/ssl_certs/sensu_ca/cacert.pem /etc/rabbitmq/ssl
+        cp /tmp/ssl_certs/server/cert.pem /etc/rabbitmq/ssl
+        cp /tmp/ssl_certs/server/key.pem /etc/rabbitmq/ssl
+      },
+      "Configuring RabbitMQ SSL Listener" => ->(k){
+        scp.upload! MONITOR_DIR.join('rabbitmq/rabbitmq.config').to_s, "/etc/rabbitmq/rabbitmq.config"
+      }
+    }
 
     def use_sensu!
       if package_installed? "sensu"
@@ -14,6 +33,11 @@ module Hyperdock
       end
     end
 
+    def configure_sensu_monitor!
+      generate_new_certificates
+      setup_rabbitmq
+    end
+
     def install_sensu!
       log "Installing sensu (monitor)"
       stream_exec(INSTALL_SCRIPT) do
@@ -21,22 +45,22 @@ module Hyperdock
       end
     end
 
-    def configure_sensu_monitor!
-      generate_new_certificates
+    def setup_rabbitmq
+      if package_installed? "rabbitmq-server"
+        execute_scripts_hash RABBITMQ_POSTINSTALL_SCRIPT, self
+      else
+        log "Installing rabbitmq-server"
+        stream_exec(RABBITMQ_INSTALL_SCRIPT) { setup_rabbitmq }
+      end
     end
 
     def generate_new_certificates
-      ssh.exec!("rm -rf /tmp/ssl_cert*")
+      ssh.exec! "rm -rf /tmp/ssl_cert*"
       log "Uploading SSL certificate generator"
       scp.upload! CERT_TAR.to_s, "/tmp"
-      script = <<-EOF
-        cd /tmp
-        tar -xvf ssl_certs.tar
-        cd ssl_certs
-        ./ssl_certs.sh generate 2>/dev/null
-      EOF
-      stream_exec(script)
+      log "Extracting ..."
+      ssh.exec! "cd /tmp && tar -xvf ssl_certs.tar"
+      ssh.exec! "cd /tmp/ssl_certs && ./ssl_certs.sh generate 2>/dev/null"
     end
-
   end
 end
