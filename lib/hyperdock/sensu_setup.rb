dfec85f201ba9b84cf35157a6680a2d5edcb9c7b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-18 18:17:57 -0700

    adjust some paths and move more into sensu common

--- lib/hyperdock/sensu_setup.rb
+++ lib/hyperdock/sensu_setup.rb
@@ -3,8 +3,6 @@ require 'hyperdock/sensu_setup_common'
 module Hyperdock
   module SensuSetup
     include SensuSetupCommon
-    RABBIT_CONF = CLIENT_DIR.join('conf.d/rabbitmq.json')
-    CLIENT_CONF = SENSU_CONFIG_DIR.join('client.json')
 
     def use_sensu!
       if package_installed? "sensu"
@@ -36,30 +34,12 @@ module Hyperdock
       log ssh.exec!("/etc/init.d/sensu-client start")
     end
 
-    def write_rabbit_config!
-      conf = JSON.parse RABBIT_CONF.read
-      conf["rabbitmq"]["password"] = ENV["RABBITMQ_PASSWORD"]
-      conf = JSON.pretty_generate(conf)
-      remote_write '/etc/sensu/conf.d/rabbitmq.json', conf
-    end
-
     def write_client_config!
       conf = JSON.parse CLIENT_CONF.read
       conf["client"]["name"] = @name
       conf["client"]["address"] = @host
       conf = JSON.pretty_generate(conf)
       remote_write '/etc/sensu/conf.d/client.json', conf
-    end
-
-    def write_sensu_client_certs!
-      ssh.exec!("rm -rf /etc/sensu/ssl ; mkdir -p /etc/sensu/ssl")
-      scp.upload! SSL_KEY.to_s, '/etc/sensu/ssl/key.pem'
-      scp.upload! SSL_CERT.to_s, '/etc/sensu/ssl/cert.pem'
-    end
-
-    def sensu_client_certs_installed?
-      out = ssh.exec!("ls /etc/sensu/ssl")
-      !!(out =~ /key.pem/) && !!(out =~ /cert.pem/)
     end
   end
 end
--- lib/hyperdock/sensu_setup_common.rb
+++ lib/hyperdock/sensu_setup_common.rb
@@ -1,7 +1,5 @@
 module Hyperdock
   module SensuSetupCommon
-    SENSU_CONFIG_DIR = Rails.root.join('config/sensu')
-    CLIENT_DIR = SENSU_CONFIG_DIR.join('client')
     INSTALL_SCRIPT = <<-EOF
       wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | sudo apt-key add -
       echo "deb     http://repos.sensuapp.org/apt sensu main" > /etc/apt/sources.list.d/sensu.list
@@ -9,12 +7,31 @@ module Hyperdock
       export DEBIAN_FRONTEND=noninteractive
       apt-get install -y sensu
     EOF
+    SENSU_CONFIG_DIR = Rails.root.join('config/sensu')
+    CLIENT_DIR = SENSU_CONFIG_DIR.join('client')
+    CLIENT_CONF = CLIENT_DIR.join('client.json')
     SSL_KEY = CLIENT_DIR.join('ssl/key.pem')
     SSL_CERT = CLIENT_DIR.join('ssl/cert.pem')
+    RABBIT_CONF = CLIENT_DIR.join('conf.d/rabbitmq.json')
+    MONITOR_DIR = SENSU_CONFIG_DIR.join('monitor')
+    REDIS_CONF = MONITOR_DIR.join('conf.d/redis.json')
 
     def use_sensu_embedded_ruby!
       remote_write '/etc/default/sensu', "EMBEDDED_RUBY=true"
     end
-    
+
+    def write_sensu_client_certs!
+      ssh.exec!("rm -rf /etc/sensu/ssl ; mkdir -p /etc/sensu/ssl")
+      scp.upload! SSL_KEY.to_s, '/etc/sensu/ssl/key.pem'
+      scp.upload! SSL_CERT.to_s, '/etc/sensu/ssl/cert.pem'
+    end
+
+    def write_rabbit_config!
+      conf = JSON.parse RABBIT_CONF.read
+      conf["rabbitmq"]["password"] = ENV["RABBITMQ_PASSWORD"]
+      conf["rabbitmq"]["host"] = ENV["RABBITMQ_HOST"]
+      conf = JSON.pretty_generate(conf)
+      remote_write '/etc/sensu/conf.d/rabbitmq.json', conf
+    end
   end
 end
