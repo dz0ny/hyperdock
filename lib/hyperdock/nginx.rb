1e230a0fa359452eabbd6e0395c2aab3cdce4477
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-22 21:35:50 -0700

    add ssl to nginx vhost builder

--- lib/hyperdock/nginx.rb
+++ lib/hyperdock/nginx.rb
@@ -7,7 +7,13 @@ module Hyperdock
       ssh.exec! "rm -f #{site_available}"
       ssh.exec! "rm -f #{site_enabled}"
       conf = Pathname.new(opts[:template_path]).read
-      remote_write site_available, conf.gsub('SERVER_NAME', opts[:server_name])
+      conf = conf.gsub('SERVER_NAME', opts[:server_name])
+      unless opts[:insecure]
+        ssl = generate_certificate(cert: "/var/ssl/nginx/cert.pem", key: "/var/ssl/nginx/key.pem")
+        conf = conf.gsub('SSL_CERT', ssl[:ssl_cert])
+        conf = conf.gsub('SSL_KEY', ssl[:ssl_key])
+      end
+      remote_write site_available, conf
       ssh.exec! "ln -s #{site_available} #{site_enabled}"
       ssh.exec! "service nginx reload"
       log_after "Be sure to add DNS record: "+"#{opts[:server_name]} to #{@host}".yellow
