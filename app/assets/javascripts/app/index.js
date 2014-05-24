b7c8715f894b02ab64c090d7a8d2332c57bee362
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-23 17:24:08 -0700

    provide App.ws() in js

--- app/assets/javascripts/app/index.js
+++ app/assets/javascripts/app/index.js
@@ -1,10 +1,18 @@
 window.App = {
   ready: function(cb){
-    $(document).ready(cb);
-    $(document).on('page:load', cb);
+    $(document).ready(cb.bind(this));
+    $(document).on('page:load', cb.bind(this));
   }
 };
 
 App.ready(function() {
   $('select').select2();
 });
+
+App.ws = function() {
+  if (typeof this.dispatcher === 'undefined') {
+    return this.dispatcher = new WebSocketRails("localhost:3000/websocket");
+  } else {
+    return this.dispatcher;
+  }
+}
