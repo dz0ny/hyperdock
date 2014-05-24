47a24d608f63339b38f1365e63e37b1c62317837
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-23 20:40:09 -0700

    refactor provisioner

--- app/workers/provisioner.rb
+++ app/workers/provisioner.rb
@@ -8,31 +8,32 @@ class Provisioner
   def perform(class_string, id)
     case class_string
     when 'Host'
-      provision_host Host.find(id), opts
+      provision_host Host.find(id), password: ''
     when 'Container'
-      provision_container Container.find(id), opts
+      provision_container Container.find(id)
     end
   end
 
-  def provision_host record, opts
+  def provision_host record, opts={}
+    logger.info "Provisioning host #{record.name} #{record.ip_address}"
+    ch = WebsocketRails["host_#{record.id}".to_sym]
     klass = record.monitor? ? MonitorProvisioner : HostProvisioner
     provisioner = klass.new(record.ip_address, 'root', opts[:password], record.name)
-    provisioner.on_output do |data|
-      ch = "host_#{record.id}".to_sym
-      WebsocketRails[ch].trigger 'provisioner', data
-    end
-    provisioner.provision_test!
+    provisioner.on_exit {|code|
+      ch.trigger 'provisioner', {success: code == 0}
+    }.on_output {|data|
+      ch.trigger 'provisioner', data
+    }.provision!
   end
 
   ##
   # You won't always pull an image when provisioning.
   # Sometimes you'll be using a local image. FIXME
-  def provision_container record, opts
-    opts[:container] = record
-    opts[:logger] = logger
-    provisioner = ContainerProvisioner.new opts
+  def provision_container record
+    provisioner = ContainerProvisioner.new container: record, logger: logger
     provisioner.provision! do |data|
-      ch = "container_#{opts[:container].id}".to_sym
+      ch = "container_#{container.id}".to_sym
+      logger.info data
       WebsocketRails[ch].trigger 'provisioner', data
     end
   end
