156b5d7405c8897aa676e0ba67a71ce6c6aa3237
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-25 04:00:52 -0700

    setup DNS and also fix a typo

--- lib/hyperdock/nginx.rb
+++ lib/hyperdock/nginx.rb
@@ -1,5 +1,11 @@
+require 'hyperdock/dns'
+
 module Hyperdock
   module Nginx
+    include DNS
+    
+    NGINX_BUCKET_SIZE_CONF = '/etc/nginx/conf.d/server_names_bucket_size.conf'
+
     def setup_nginx_vhost opts
       ssh.exec! 'rm -f /etc/nginx/sites-enabled/default'
       site_available = "/etc/nginx/sites-available/#{opts[:site_name]}"
@@ -10,13 +16,17 @@ module Hyperdock
       conf = conf.gsub('SERVER_NAME', opts[:server_name])
       unless opts[:insecure]
         ssl = generate_certificate(cert: "/var/ssl/nginx/cert.pem", key: "/var/ssl/nginx/key.pem")
-        conf = conf.gsub('SSL_CERT', ssl[:ssl_cert])
-        conf = conf.gsub('SSL_KEY', ssl[:ssl_key])
+        conf = conf.gsub('SSL_CERT', ssl[:cert])
+        conf = conf.gsub('SSL_KEY', ssl[:key])
       end
       remote_write site_available, conf
       ssh.exec! "ln -s #{site_available} #{site_enabled}"
       ssh.exec! "service nginx reload"
-      log_after "Be sure to add DNS record: "+"#{opts[:server_name]} to #{@host}".yellow
+      add_dns_record(:A, opts[:server_name], @host)
+      log_after("Created site https://#{opts[:server_name]}")
+      unless file_exists? NGINX_BUCKET_SIZE_CONF
+        remote_write NGINX_BUCKET_SIZE_CONF, "server_names_hash_bucket_size 64;"
+      end
     end
   end
 end
