6d3ac057ebec8b9050982877d6d4396056abb7c6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-26 15:03:56 -0700

    use the tokens in websocket controller

--- app/controllers/websocket_controller.rb
+++ app/controllers/websocket_controller.rb
@@ -1,20 +1,19 @@
 class WebsocketController < WebsocketRails::BaseController
   def provision_host
-    if current_user.admin?
+    if user.admin?
       Provisioner.perform_async('Host', event.data['id'], password: event.data['password'])
     end
   end
 
   def reset_known_hosts
-    if current_user.admin?
+    if user.admin?
       h = Host.find(event.data['id'])
-      h.ssh_known_hosts = ""
-      h.save
+      h.update_column(:ssh_known_hosts, '')
     end
   end
 
   def list_containers
-    if current_user.admin?
+    if user.admin?
       h = Host.find(event.data['id'])
       ch = WebsocketRails["host_#{h.id}".to_sym]
       ch.trigger 'provisioner', { event: 'stdout', message: h.remote_containers.to_json }
@@ -22,12 +21,17 @@ class WebsocketController < WebsocketRails::BaseController
   end
 
   def get_host_info
-    if current_user.admin?
+    if user.admin?
       h = Host.find(event.data['id'])
       ch = WebsocketRails["host_#{h.id}".to_sym]
       JSON.pretty_generate(h.get_info).each_line do |line|
         ch.trigger 'provisioner', { event: 'stdout', message: line.chomp }
       end
     end
+  end
+
+  private
+  def user
+    @user ||= current_user || User.find_by_auth_token(event.data['user_token'])
   end
 end
