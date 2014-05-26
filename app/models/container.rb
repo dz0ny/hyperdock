c2904b6cc1bc812c080945933674f838abc38a3a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-26 16:25:47 -0700

    fix container deploy

--- app/models/container.rb
+++ app/models/container.rb
@@ -83,7 +83,7 @@ class Container < ActiveRecord::Base
   def select_host
     unless self.host
       # TODO: select most optimal host
-      self.host = self.region.hosts.where(is_monitor: false)
+      self.host = self.region.hosts.where(is_monitor: false).first
     end
   end
 
