b87c57b0d6f4d2d0ad30cff83d6a5b893ecbb92c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-05-31 17:43:11 -0700

    cleanup SSH concern

--- app/models/concerns/secure_shell_identity.rb
+++ app/models/concerns/secure_shell_identity.rb
@@ -2,15 +2,16 @@ module SecureShellIdentity
   extend ActiveSupport::Concern
 
   def generate_ssh_identity
+    ssh_auth_files.values {|file| FileUtils.rm(file) if file.exist? }
     `yes | ssh-keygen -t rsa -f #{ssh_auth_files[:private_key]} -N ""`
     self.ssh_identity = ssh_auth_files
     self.save!
   end
 
   def ssh_auth_files
-    ident = { private_key: tmp.join("id_rsa"),
-              public_key: tmp.join("id_rsa.pub"),
-              known_hosts: tmp.join("known_hosts") }
+    @ssh_auth_files ||= { private_key: tmp.join("id_rsa"),
+                          public_key: tmp.join("id_rsa.pub"),
+                          known_hosts: tmp.join("known_hosts") }
   end
 
   def ssh_identity
@@ -23,8 +24,8 @@ module SecureShellIdentity
   end
 
   def ssh_identity= ident
-    self.ssh_private_key = ident[:private_key].read 
-    self.ssh_public_key = ident[:public_key].read
+    self.ssh_private_key = ident[:private_key].read.strip
+    self.ssh_public_key = ident[:public_key].read.strip
     self.ssh_known_hosts = ident[:known_hosts].read rescue ""
     self.save!
   end
